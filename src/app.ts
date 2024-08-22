9e40d5707ec7c4c41183594e9d040ff2bdf20afd
Pabl03s <60926999+PabloVirgolini@users.noreply.github.com>
2024-08-21 23:22:43 -0300

    agregamos CORS

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express, { NextFunction, Request, Response } from 'express'
+import cors from 'cors';
 import { tipovolqueteRouter } from './tipovolquete/tipovolquete.routes.js'
 import { volqueteRouter } from './volquete/volquete.routes.js'
-const app = express()
-app.use(express.json())
 
-app.use('/api/tipovolquetes',tipovolqueteRouter)
-app.use('/api/volquetes',volqueteRouter)
+const app = express();
+
+app.use(express.json());
+app.use(cors({origin:'http://localhost:4200'}));
+
+app.use('/api/tipovolquetes',tipovolqueteRouter);
+app.use('/api/volquetes',volqueteRouter);
 
 // DEFAULT
 app.use((_,res)=>{
     return res.status(400).send({message:'Resource not found'})
- })
+ });
  
  
  
